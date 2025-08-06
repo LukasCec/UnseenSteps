30e50be49fe89d49c276adc27eb0f587c5909626
Lukas Cec <lukinecka123987@gmail.com>
2025-08-06 17:26:45 +0200

    Level Border reset

---
+++ Assets/_Scripts/ResetOnTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ResetOnTrigger : MonoBehaviour
+{
+    void Awake()
+    {
+        
+        var bc = GetComponent<BoxCollider2D>();
+        bc.isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      
+        if (other.CompareTag("Player"))
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
