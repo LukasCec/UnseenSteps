e9b2c0143eb60bf2ce763d5c0c42d3cc1f28ab9e
Lukas Cec <lukinecka123987@gmail.com>
2025-08-09 17:19:00 +0200

    [INVENTORY] Scribltable game object

---
+++ Assets/_Scripts/InventoryManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryData", menuName = "Game/Inventory Data")]
+public class InventoryData : ScriptableObject
+{
+    [Header("Inventory")]
+    public int healPotions;
+    public int revealPotions;
+    public int coins;
+
+    public void AddHealPotion(int amount = 1)
+    {
+        healPotions = Mathf.Max(0, healPotions + amount);
+    }
+
+    public void AddRevealPotion(int amount = 1)
+    {
+        revealPotions = Mathf.Max(0, revealPotions + amount);
+    }
+
+    public void AddCoins(int amount = 1)
+    {
+        coins = Mathf.Max(0, coins + amount);
+    }
+
+    public bool UseHealPotion()
+    {
+        if (healPotions > 0)
+        {
+            healPotions--;
+            return true;
+        }
+        return false;
+    }
+
+    public bool UseRevealPotion()
+    {
+        if (revealPotions > 0)
+        {
+            revealPotions--;
+            return true;
+        }
+        return false;
+    }
+
+    public bool SpendCoins(int amount)
+    {
+        if (coins >= amount)
+        {
+            coins -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void ResetInventory()
+    {
+        healPotions = 0;
+        revealPotions = 0;
+        coins = 0;
+    }
+}
